637a09e46433378ce0c056646e090722523b9840
Moe <71136219+moe-light@users.noreply.github.com>
2021-10-28 03:17:21 +0330

    Update bot-boy-test.py

--- bot-boy-test.py
+++ bot-boy-test.py
@@ -6,16 +6,17 @@ token = os.environ['API_Token']
 
 bot = telebot.TeleBot(token)
 
+markup = types.ReplyKeyboardMarkup()
+itembtna = types.KeyboardButton('a')
+itembtnv = types.KeyboardButton('v')
+itembtnc = types.KeyboardButton('c')
+itembtnd = types.KeyboardButton('d')
+itembtne = types.KeyboardButton('e')
+markup.row(itembtna, itembtnv)
+markup.row(itembtnc, itembtnd, itembtne)
+
 @bot.message_handler(commands=['start'])
 def start(msg):
-  markup = types.ReplyKeyboardMarkup()
-  itembtna = types.KeyboardButton('a')
-  itembtnv = types.KeyboardButton('v')
-  itembtnc = types.KeyboardButton('c')
-  itembtnd = types.KeyboardButton('d')
-  itembtne = types.KeyboardButton('e')
-  markup.row(itembtna, itembtnv)
-  markup.row(itembtnc, itembtnd, itembtne)
   bot.reply_to(msg, 'سلام من هنوز زندم')
 
 
