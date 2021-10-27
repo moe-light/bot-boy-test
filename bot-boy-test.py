2ed7888be85920ac122996d3661d29fb6df3a6f9
Moe <71136219+moe-light@users.noreply.github.com>
2021-10-28 03:15:43 +0330

    Update bot-boy-test.py

--- bot-boy-test.py
+++ bot-boy-test.py
@@ -8,11 +8,15 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(msg):
+  markup = types.ReplyKeyboardMarkup()
+  itembtna = types.KeyboardButton('a')
+  itembtnv = types.KeyboardButton('v')
+  itembtnc = types.KeyboardButton('c')
+  itembtnd = types.KeyboardButton('d')
+  itembtne = types.KeyboardButton('e')
+  markup.row(itembtna, itembtnv)
+  markup.row(itembtnc, itembtnd, itembtne)
   bot.reply_to(msg, 'سلام من هنوز زندم')
-  markup = types.ReplyKeyboardMarkup(row_width=2)
-  itembtn1 = types.KeyboardButton('salam')
-  itembtn2 = types.KeyboardButton('oodafez')
-  markup.add(itembtn1, itembtn2)
 
 
 @bot.message_handler(func=lambda m: True)
