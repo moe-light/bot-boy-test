18784882f1661382b248a668cabcfa9a65816bbb
Moe <71136219+moe-light@users.noreply.github.com>
2021-10-28 03:13:18 +0330

    Update bot-boy-test.py

--- bot-boy-test.py
+++ bot-boy-test.py
@@ -8,11 +8,11 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(msg):
+  bot.reply_to(msg, 'سلام من هنوز زندم')
   markup = types.ReplyKeyboardMarkup(row_width=2)
   itembtn1 = types.KeyboardButton('salam')
   itembtn2 = types.KeyboardButton('oodafez')
   markup.add(itembtn1, itembtn2)
-  bot.send_message(chat_id, "Choose one letter:", reply_markup=markup)
 
 
 @bot.message_handler(func=lambda m: True)
