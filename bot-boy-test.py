65de0a6932eb663563d398f163ffbc2af172f9ba
Moe <71136219+moe-light@users.noreply.github.com>
2021-10-28 02:59:19 +0330

    Update bot-boy-test.py

--- bot-boy-test.py
+++ bot-boy-test.py
@@ -18,6 +18,6 @@ markup = types.ReplyKeyboardMarkup(row_width=2)
 itembtn1 = types.KeyboardButton('salam')
 itembtn2 = types.KeyboardButton('oodafez')
 markup.add(itembtn1, itembtn2)
-tb.send_message(chat_id, "Choose one letter:", reply_markup=markup)
+bot.send_message(chat_id, "Choose one letter:", reply_markup=markup)
 
 bot.infinity_polling()
